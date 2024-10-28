94fab42a51e670f31751ed3f6dccb924074d77ae
Conner685 <cponton@my.bcit.ca>
2024-10-28 14:22:54 -0700

    Created pop up login functionality with certain CSS and design choices available for editting

--- scripts/script.js
+++ scripts/script.js
@@ -9,3 +9,21 @@ function logout() {
         // An error happened.
       });
 }
+
+document.getElementById('signup-btn').addEventListener('click',
+  function() {
+  var signupContainer = document.getElementById('signup-container'); 
+  var closeButton = document.createElement('span');
+  var body = document.getElementById('core');
+  signupContainer.style = ''
+  closeButton.className = 'close-btn';
+  closeButton.innerHTML = '&times;';
+  signupContainer.appendChild(closeButton);
+  body.style = 'overflow: hidden;'
+
+  closeButton.addEventListener('click', function() {
+  signupContainer.removeChild(closeButton);
+  signupContainer.style = 'display: none;'
+  body.style = 'overflow: none;'
+  });
+  });
