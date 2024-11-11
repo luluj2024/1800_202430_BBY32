12b4788045c9d3379ae06159bbd2d06b8e29c168
Conner685 <cponton@my.bcit.ca>
2024-11-11 12:07:44 -0800

    Cleaned up console logs and improved code readability

--- scripts/favorites.js
+++ scripts/favorites.js
@@ -1,19 +1,17 @@
 let currentUserId;
 
+//authenticates current user
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
         currentUserId = user.uid;
-
-        // you have to call it here because firebase 
-        // needs some time to figure out if the user is logged in
-        // once it is it sets the currentUserId, but because javascript is
-        // asynchronous??? or something it goes ahead without "waiting" for it to finish checking
+        //Calls all routes after delay and initializes searchbar 
         processLoad();
         let searchbar = document.getElementById("searchbar");
         searchbar.value = "";
     }
 })
 
+//Displays all routes in users favorites
 function displayAllRoutes() {
     let busTemplate = document.getElementById("bus-template");
     let container = document.getElementById("bus-info");
@@ -21,7 +19,6 @@ function displayAllRoutes() {
     document.getElementById("status").innerHTML = '';
 
     let favCheck = false;
-    
     db.collection("users").doc(currentUserId).get().then(user => {
         let favoriteRoutes = user.data().favorite_routes;
         if (favoriteRoutes.length == 0) {
@@ -30,7 +27,6 @@ function displayAllRoutes() {
         else {
         db.collection("Routes").get().then(routeList => {
             routeList.forEach(routeId => {
-                // let favoriteRoutes = user.data().favorite_routes;
                 favCheck = favoriteRoutes.includes(routeId.id);
                 if (favCheck) {
                     outputCards(container, busTemplate, routeId);
@@ -40,16 +36,12 @@ function displayAllRoutes() {
     }
     })
 }
-
-// displayAllRoutes(); <------------------ You cannot call this here because of the asynchronous nature of javascript or something  
-
-//Only displays routes similar to search query 
+ 
+//Only displays routes similar to search query that are favorited by the user
 function displaySimilarRoutes() {
     searchbar = document.getElementById("searchbar");
-    // console.log(searchbar.value);
     if (searchbar.value == "") {
         displayAllRoutes();
-        // console.log("reset");
     }
     else {
         let count = 0;
@@ -60,26 +52,22 @@ function displaySimilarRoutes() {
         let favCheck = false;
         db.collection("users").doc(currentUserId).get().then(user => {
             let favoriteRoutes = user.data().favorite_routes;
+            //Checks users favorited routes and displays them accordingly 
             if (favoriteRoutes.length == 0) {
                 document.getElementById("status").innerHTML = "<h4>Favorite some routes to meet commute buddies!</h4>";
             }
             else {
                 db.collection("Routes").get().then(routeList => {
                     routeList.forEach(routeId => {
-                        // console.log(routeId.data().bus);
-                        // console.log(searchbar.value);
-    
                         favCheck = favoriteRoutes.includes(routeId.id);
                         if (favCheck) {
                             if (relatedRoutes(searchbar.value.toLowerCase(), routeId.data().bus, routeId.data().name)) {
                                 count += 1;
-                                // console.log(count + "e")
                                 outputCards(container, busTemplate, routeId);
                             }
                         }
                     })
                 }).then(() => {
-                    // console.log("final" + count)
                     if (count == 0) {
                         document.getElementById("status").innerHTML = "<h4>Sorry, your search doesnt match any routes in your favorites.</h4>";
                     }
@@ -92,6 +80,7 @@ function displaySimilarRoutes() {
     }
 }
 
+//Compares search query with favorite routes list
 function relatedRoutes(search, result, result2) {
     result += '';
     result2 += '';
@@ -103,22 +92,18 @@ function relatedRoutes(search, result, result2) {
         }
         else if (i >= result.length) {
             if (search[i] != result2[i].toLowerCase()) {
-                // console.log("fail");
                 return false;
             }
         }
         else if (i >= result2.length) {
             if (search[i] != result[i]) {
-                // console.log("fail");
                 return false;
             }
         }
         else if (search[i] != result[i] && search[i].toLowerCase() != result2[i].toLowerCase()) {
-            // console.log("fail");
             return false;
         }
     }
-    // console.log(search + " success " + result);
     return true;
 }
 
@@ -147,9 +132,8 @@ function debounce(func, timeout = 250) {
 const processSearch = debounce(() => displaySimilarRoutes());
 const processLoad = debounce(() => displayAllRoutes());
 
-
+//Allows users to unfavorite routes from favorites page
 async function unfavoriteRoute(route) {
-    // console.log("entered unfavorites");
     let userDocRef = await db.collection("users").doc(currentUserId);
     let routeDocRef = await db.collection("Routes").doc(route);
     userDocRef.update({
