7c8884f7ca0902d50ea7eb84a9d8787a63c82ce6
lulu <lulujiang505@gmail.com>
2024-11-18 14:42:05 -0800

    Changed the profile page's color

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,3 +1,7 @@
+/*
+  Dynamically adds an "active" class to the navigation item that 
+  matches the current page's URL, highlighting it to indicate the active page.
+*/
 $(document).ready(function () {
     const currentPath = window.location.pathname;
     const navItems = document.querySelectorAll(".custom-nav-item");
